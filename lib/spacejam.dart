75c9265650f98a9d6402294e9817fbcf470501a5
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2022-01-21 19:35:11 +0100

    added reference for new elements

--- lib/spacejam.dart
+++ lib/spacejam.dart
@@ -3,4 +3,6 @@ export "src/theme/text_styles.dart";
 // Widgets
 export "src/contentbox/content_box.dart";
 export "src/imagepage/image_page.dart";
+export "src/appbar/appbar_action.dart";
+export "src/appbar/appbar.dart";
 export "src/button/button.dart";
