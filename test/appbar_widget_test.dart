bf18805b56e37ca41670d982226c258ac02f812b
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2022-01-21 19:43:31 +0100

    test for appbar widget

---
+++ test/appbar_widget_test.dart
@@ -0,0 +1,35 @@
+// Flutter
+import "package:flutter_test/flutter_test.dart";
+import "package:flutter/material.dart";
+
+import "package:spacejam/spacejam.dart";
+
+void main() {
+  group("Testing the appbar widget", () {
+// Appbar
+    testWidgets("AppBar", (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Appbar(
+            title: "title",
+            subtitle: "subtitle",
+            leftAction: AppBarAction(icon: Icons.add),
+            rightAction: AppBarAction(icon: Icons.remove),
+          ),
+        ),
+      );
+
+// Search elements
+      final Finder titleFinder = find.text("title");
+      final Finder subtitleFinder = find.text("subtitle");
+      final Finder leftIconFinder = find.byIcon(Icons.add);
+      final Finder rightIconFinder = find.byIcon(Icons.remove);
+
+// Verify
+      expect(titleFinder, findsOneWidget);
+      expect(subtitleFinder, findsOneWidget);
+      expect(leftIconFinder, findsOneWidget);
+      expect(rightIconFinder, findsOneWidget);
+    });
+  });
+}
